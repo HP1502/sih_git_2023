f3a01261f6f7b359b5c6d289d1330c5942be6231
HP1502 <121014934+HP1502@users.noreply.github.com>
2024-12-12 18:14:52 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -20,7 +20,7 @@ if 'generated' not in st.session_state:
 if 'past' not in st.session_state:
     st.session_state['past'] = []
 def get_text():
-    input_text = st.text_input("You: ","Hello, how are you?", key="input")
+    input_text = st.text_input("You: ","Hello, how are you? ", key="input")
     return input_text
 user_input = get_text()
 
